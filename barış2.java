e9907e3852b9500f46d4966f3df1ab448de4d5a4
BRC1234210 <askbarrcan41@gmail.com>
2025-09-16 20:15:26 +0300

    created

--- barış2.java
+++ barış2.java
@@ -1,7 +1,7 @@
 package JavaProje06.Bank_Project_Cozum;
 
-public class Barış {
+public class barış2 {
     public static void main(String[] args) {
-        System.out.println("barış burdaaa ");
+        System.out.println("barış hep burada");
     }
 }
